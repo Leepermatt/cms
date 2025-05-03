900ed1f0e010ba4c05b59585e67ee357c8abab51
Leepermatt <131700890+Leepermatt@users.noreply.github.com>
2025-05-02 21:20:13 -0700

    week 2

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-list',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './contact-list.component.css'
 })
 export class ContactListComponent {
+  contacts: Contact[] = [
+    new Contact('1', 'John Doe', '123@byui.com', '208-555-1234', 'https://www.w3schools.com/howto/img_avatar.png', []),
+    new Contact('2', 'Jane Smith', 'barzeer@byui,edu', '208-555-5678', 'https://www.w3schools.com/howto/img_avatar.png', []),
+  ];
+  
 
 }
