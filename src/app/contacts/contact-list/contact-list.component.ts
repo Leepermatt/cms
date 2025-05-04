519ac3257ab206793c80b170849ed0d5c7bef061
Leepermatt <131700890+Leepermatt@users.noreply.github.com>
2025-05-03 17:42:34 -0700

    week2.3

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -9,9 +9,8 @@ import { Contact } from '../contact.model';
 })
 export class ContactListComponent {
   contacts: Contact[] = [
-    new Contact('1', 'John Doe', '123@byui.com', '208-555-1234', 'https://www.w3schools.com/howto/img_avatar.png', []),
-    new Contact('2', 'Jane Smith', 'barzeer@byui,edu', '208-555-5678', 'https://www.w3schools.com/howto/img_avatar.png', []),
+    new Contact('1', 'R. Kent Jackson', '123@byui.com', '208-555-1234', 'images/barzeer.jpg', []),
+    new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-555-5678', 'images/jacksonk.jpg', []),
   ];
-  
 
 }
